0f71bf5876b7bf98b9099dd5f88d5d4bce81b314
godiva-frappuccino <miki.10pro@gmail.com>
2020-10-30 17:53:59 +0900

    implement reading gyro

---
+++ Assets/Scripts/MoveObject/MoveBySensor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBySensor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      var pos = transform.position;
+      Vector3 m_accel = Input.acceleration;
+      Input.gyro.enabled = true;
+      Gyroscope m_gyro = Input.gyro;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      var pos = transform.position;
+      Vector3 m_accel = Input.acceleration;
+      Gyroscope m_gyro = Input.gyro;
+
+      if(false)
+      {
+        Debug.Log("Accel: " + m_accel);
+        var moveX = m_accel.x * Time.deltaTime * Time.deltaTime;
+        var moveY = m_accel.y * Time.deltaTime * Time.deltaTime;
+        var moveZ = m_accel.z * Time.deltaTime * Time.deltaTime;
+        pos.x += moveX;
+        pos.y += moveY;
+        pos.z += moveZ;
+        transform.position = pos;
+      }
+      if(m_gyro != null)
+      {
+        Debug.Log("Gyro: " + m_gyro.attitude);
+        float x = -m_gyro.attitude.x;
+        float y = -m_gyro.attitude.y;
+        float z = m_gyro.attitude.z;
+        float w = m_gyro.attitude.w;
+        //transform.rotation = m_gyro.attitude;
+        // adjust rotation to screen
+        /*
+        transform.rotation = new Quaternion(x, y, z, w);
+        transform.Rotate(new Vector3(1, 0, 0), 90);
+        */
+        //transform.rotation = m_gyro.attitude;
+        transform.rotation = Quaternion.Euler(90, 0, 0) * (new Quaternion(x, y, z, w));
+
+      }
+    }
+}
